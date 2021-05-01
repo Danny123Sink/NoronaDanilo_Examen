c29a79fabacea10f1685f7fe067253622855cbf4
Danilo Noroña <dannyepacrew01@gmail.com>
2021-04-30 21:47:40 -0500

    Agregar archivos de proyecto.

---
+++ NoronaDanilo_Examen/Login.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NoronaDanilo_Examen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private async void BtnAbrir_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                string Usuario = txtUsusario.Text;
+                string Password = txtPass.Text;
+
+                if (Usuario == "estudiante2021" && Password == "uisrael2021")
+                {
+                    await Navigation.PushAsync(new Registro(Usuario, Password));
+                }
+
+                else
+                {
+                    string mensaje1 = " Dato Incorrecto";
+                    DisplayAlert("Mensaje de alerta", mensaje1, "Gracias");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                DisplayAlert("Mensaje de alerta", "ERROR" + ex.Message, "ok");
+
+                throw;
+            }
+        }
+  }
+}
---
+++ NoronaDanilo_Examen/Registro.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NoronaDanilo_Examen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro(string Usuario, string Password)
+        {
+            InitializeComponent();
+
+            string usuario = lblUsuario.Text;
+            lblUsuario.Text = usuario + Usuario;
+        }
+
+        private void btnCalcularMensual_Clicked(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            double resultado;
+            double total;
+            try
+            {
+                double monto = Convert.ToDouble(txtMontoInicial.Text);
+                if (monto <= 1800 && monto > 0)
+                {
+                    resultado = ((1800 - monto) / 3) + 90;
+                    txtPagoMensual.Text = resultado.ToString();
+                    total = (resultado * 3) + monto;
+                    txtTotal.Text = total.ToString();
+
+                    string mensaje1 = "elemento guardado con exito";
+                    DisplayAlert("Mensaje de alerta", mensaje1, "Gracias");
+
+                }
+                else
+                {
+                    DisplayAlert("Alerta", "ERROR", "Gracias");
+                }
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Alerta", "ERROR" + ex.Message, "Gracias");
+                throw;
+
+            }
+
+        }
+
+        private async void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string total = txtTotal.Text;
+            string estado = lblUsuario.Text;
+
+            await Navigation.PushAsync(new Resumen(nombre, total, estado));
+        }
+    }
+}
